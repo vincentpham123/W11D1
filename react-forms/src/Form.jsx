c292f01a2feca859b171dc1cae39a7466e077c68
vincentpham123 <vincentpham32@gmail.com>
2023-06-05 14:57:24 -0700

    finished validations

--- react-forms/src/Form.jsx
+++ react-forms/src/Form.jsx
@@ -1,6 +1,40 @@
-
+import { useState } from "react";
 const Form=()=>{
-    
+    const [user,setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        phonetype: "",
+        instructor: "",
+        bio: "",
+        newsletter: ""
+    });
+    const [errors,setErrors] = useState([]);
+    const validate = () => {
+        let errors = [];
+        if(user.name.length == 0){
+            errors.push("Name can't be blank");
+        }
+        let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        if(!regex.test(user.email)){
+            errors.push("Invalid email address")
+        }
+        if(user.email.length == 0){
+            errors.push("Email can't be blank");
+        }
+        if(!/^[2-9]\d{2}-\d{3}-\d{4}$/.test(user.phone)){
+            errors.push("Invalid phone format")
+        }
+        else{
+            if(!user.phonetype){
+                errors.push("Select Phone Type");
+            }
+        }
+        if(user.bio.length > 280){
+            errors.push("Over character limit");
+        }
+    }
+
     return(
         <>
         <h1>Registration Form</h1>
@@ -42,20 +76,6 @@ const Form=()=>{
             </label>
             <br></br>
             <button>Submit</button>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </form>
         </>
     )
